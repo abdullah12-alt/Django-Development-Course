0003343ca611a0c33f8fc2a72d98574da4d61110
Abdullah <hassanabdullah@gmail.com>
2024-05-19 11:43:49 +0500

    templates

--- DjangoApp/DjangoProject/app1/urls.py
+++ DjangoApp/DjangoProject/app1/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path("home/",index ,name="index"),
+    path("about/",about,name="about"),
+    path("contact/",contact,name="contact")
+    
 ]
--- DjangoApp/DjangoProject/app1/views.py
+++ DjangoApp/DjangoProject/app1/views.py
@@ -14,3 +14,21 @@ def index(request):
     
     context={"employe":employe}
     return render(request,'home.html',context)
+
+def about(request):
+    head="Welcome To Minhaj University Lahore"
+    para="""Minhaj University Lahore was founded in 1986 by Shaykh-ul-Islam Prof. Dr Muhammad Tahir-ul-Qadri, patron-in-chief of Minhaj ul Quran International. It is located in a significant place which is easily approachable from all the main areas of the city. Its campuses are situated at Township & Model Town. Degree awarding status was granted by Govt. of the Punjab vide Act No: XII of 2005. The Higher Education Commission of Pakistan has also recognized Minhaj University, as ‘W3‘ ranking University.
+
+It comprises of nine faculties that of Computer Science & Information Technology, Basic Sciences & Mathematics, Economics & Management Sciences, Social Sciences & Humanities, Languages, Shariah & Islamic Studies, Engineering, Allied Health Sciences, Applied Sciences.
+    """
+    context={
+        "head":head,
+        "para":para
+    }
+    return render(request,"about.html",context)
+
+
+
+def contact(request):
+    return render(request,'contact.html',{})
+    
