a74272af0cf6ad1649dd43f6cc2dab20b4571bf7
Abdullah <hassanabdullah@gmail.com>
2024-05-30 10:21:14 +0500

    forms.py

--- FirstProjectVenv/Firstproject/Base/urls.py
+++ FirstProjectVenv/Firstproject/Base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('post/', post, name="post"),
-    path('post-detail/<int:id>', details, name="post-details")
+    path('post-detail/<int:id>', details, name="post-details"),
+    path('create-post/', create_post, name="create-post")
 ]
--- FirstProjectVenv/Firstproject/Base/views.py
+++ FirstProjectVenv/Firstproject/Base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
-
+from .forms import PostForm
 def post(request):
     post=Post.objects.all()
     context={'post':post}
@@ -15,3 +15,18 @@ def details(request,id):
     context={"post_detail":post_detail}
     return render(request,"details.html",context)
     
+    
+    
+def create_post(request):
+    if request.method=='POST':
+        form=PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        post=Post.objects.all()
+        context={'post':post}
+        return render(request,"post.html",context)
+    else:
+        form=PostForm()
+    return render(request,"create-post.html",{"form":form})
+
+
