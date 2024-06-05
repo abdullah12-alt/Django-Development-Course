14d2de2aabeed60e04df3adf272ab339bf076ba4
Abdullah <hassanabdullah@gmail.com>
2024-06-05 10:50:53 +0500

    register

--- FirstProjectVenv/Firstproject/Base/forms.py
+++ FirstProjectVenv/Firstproject/Base/forms.py
@@ -1,9 +1,18 @@
-from django.forms import ModelForm
+from django import forms
+from forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Post
-
-
 
 class PostForm(ModelForm):
     class Meta:
         model=Post
         fields="__all__"
+
+
+
+class RegistrationForm(UserCreationForm):
+    email= forms.EmailField(required="True")
+    class Meta:
+        model=User
+        fields=["username","email","password1","password2"]
--- FirstProjectVenv/Firstproject/Base/views.py
+++ FirstProjectVenv/Firstproject/Base/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
-# Create your views here.
 from .forms import PostForm
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistrationForm
+
+
+
+def register(request):
+    if request.method=='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get("username")
+            password=form.cleaned_data.get("password1")
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return redirect("post")
+    else:
+        form = RegistrationForm()
+        return render(request,"register.html",{"form":form})
+            
+
+
+
+
+
+
+
+
+
+
+
 def post(request):
     post=Post.objects.all()
     context={'post':post}
