01d956879816eb563b23a51418b65816670df4e7
Abdullah <hassanabdullah@gmail.com>
2024-06-11 11:17:35 +0500

    cart and details

--- EcomerceEnv/Ecomerce/shop/urls.py
+++ EcomerceEnv/Ecomerce/shop/urls.py
@@ -1,6 +1,8 @@
-from .views import index
-from django.urls import path,include
+from .views import index,product_detail,add_to_cart
+from django.urls import path
 
 urlpatterns = [
-    path("",index,name="product")
+    path("",index,name="product"),
+    path("product/<int:id>",product_detail,name="product_detail"),
+    path("add_to_cart/<int:id>",add_to_cart,name="add_to_cart")
 ]
--- EcomerceEnv/Ecomerce/shop/views.py
+++ EcomerceEnv/Ecomerce/shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product,Cart,CartItem,Category
 # Create your views here.
 
@@ -6,4 +6,24 @@ def index(request):
     product=Product.objects.all()
     return render(request,"index.html",{"product":product})
 
+
+
+def product_detail(request,id):
+    product=Product.objects.get(id=id)
+    return render(request,"product_detail.html",{"product":product})
+
+
+def add_to_cart(request,id):
+    product=Product.objects.get(id=id)
+    cart,created=Cart.objects.get_or_create(user=request.user)
+    cart_item,created=CartItem.objects.get_or_create(cart=cart,product=product)
+    if not created:
+        cart_item.quantity+=1
+        cart_item.save()
+        return render(request,"cart.html",{"cart":cart})    
+    return render(request,"cart.html",{"cart":cart})
+
+
+
+    
 
